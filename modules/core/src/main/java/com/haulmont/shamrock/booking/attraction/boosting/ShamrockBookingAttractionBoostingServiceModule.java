4422ae68b148fb889fd0710bd3f9f65e75922cc9
root <root@shamrock-dev-environment-service-v1-7cdf45697-2nd8n>
2025-08-20 14:00:48 +0100

    Initial commit

---
+++ modules/core/src/main/java/com/haulmont/shamrock/booking/attraction/boosting/ShamrockBookingAttractionBoostingServiceModule.java
@@ -0,0 +1,23 @@
+package com.haulmont.shamrock.booking.attraction.boosting;
+
+import com.haulmont.monaco.annotations.Module;
+import com.haulmont.monaco.container.ModuleLoader;
+
+@Module(
+        name = "shamrock-booking-attraction-boosting-service-module",
+        depends = {
+                "monaco-jetty",
+                "monaco-core",
+                "monaco-config",
+                "monaco-graylog-reporter",
+                "monaco-sentry-reporter",
+                "monaco-ds", "monaco-ds-postgresql", "monaco-ds-sybase", "monaco-metrics", "monaco-mybatis", "monaco-redis", "monaco-rs", "monaco-scheduler", "monaco-sql2o", "monaco-unirest"
+        }
+)
+public class ShamrockBookingAttractionBoostingServiceModule extends ModuleLoader {
+
+    public ShamrockBookingAttractionBoostingServiceModule () {
+        super();
+        packages(ShamrockBookingAttractionBoostingServiceModule.class.getPackageName());
+    }
+}
